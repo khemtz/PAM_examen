e45e8e7ae5d2b79d75c8aaa4346960cd96fa886b
Angel V <angel.villalvazodi@anahuac.mx>
2022-02-13 13:21:08 -0600

    Agregue calcular menor, mayor y average

--- examen.dart
+++ examen.dart
@@ -1,14 +1,18 @@
 void main() {
-  var array_numeros = [22, 44, 5, 3, 6, 99, 12, 45, 87, 23, 22, 12];
+  var arrayNumeros = [22, 44, 5, 3, 6, 99, 12, 45, 87, 23, 22, 12];
   var suma = 0;
-
-  //procedo a leer el contenido de cada elemento del arreglo
-
-  for (int i = 0; i < array_numeros.length; i++) {
-    print(i.toString() + " ->" + array_numeros[i].toString());
-    suma = suma + array_numeros[i];
+  var tamanoArray = arrayNumeros.length;
+  var mayor = 0;
+  var menor = 999999;
+  for (int indice=0; indice < tamanoArray; indice++) {
+    if (arrayNumeros[indice] > mayor) {
+      mayor = arrayNumeros[indice];      
+    }
+    if (arrayNumeros[indice] < menor) {
+      menor = arrayNumeros[indice];
+    }
+    suma = suma + arrayNumeros[indice];
   }
-
-  print(array_numeros);
-  print("la suma es:" + suma.toString());
+  var resultado = suma/tamanoArray;
+  print ("mayor es:" + mayor.toString() + "\nmenor es:"+ menor.toString() + "\navg es: "+ resultado.toString());
 }
